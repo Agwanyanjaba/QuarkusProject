bc960922aee48482d5eb7c43bfcb5b13b3294ba5
agwanyanjaba <bonkewycliffe@yahoo.com>
2023-01-05 08:36:59 +0300

    Add Transaction Model

--- src/main/java/org/wycliffe/controller/TransactionController.java
+++ src/main/java/org/wycliffe/controller/TransactionController.java
@@ -1,4 +1,20 @@
 package org.wycliffe.controller;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/v1/transactions")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+
 public class TransactionController {
+    @POST
+    @Path("/credit")
+    public Response addTransaction(TransactionRequest request){
+
+    }
 }
